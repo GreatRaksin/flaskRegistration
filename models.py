fd256f77fbb49ba9b945dc07a3d88a1dbae62c1e
demidraksin <greatraksin@icloud.com>
2021-04-13 19:27:28 +0300

    ⚡✨️Adding user model✨⚡️

--- models.py
+++ models.py
@@ -1,1 +1,9 @@
 from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    phone = db.Column(db.String(12), unique=True)
+    password = db.Column(db.String(255))
